6d2a1fa57f116b3e98749a3e4de444aa3b3ff344
Kshitiz 'Kris' Krishna <kkrishnainsys.com@Kshitizs-MacBook-Pro.local>
2017-05-17 06:17:47 -0600

    change

--- common/src/main/bash/build_and_upload.sh
+++ common/src/main/bash/build_and_upload.sh
@@ -10,7 +10,7 @@ __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Additional Maven Args [${MAVEN_ARGS}]"
 
 if [[ "${PROJECT_TYPE}" == "MAVEN" ]]; then
-    ./mvnw versions:set -DnewVersion=${PIPELINE_VERSION} ${MAVEN_ARGS}
+   # ./mvnw versions:set -DnewVersion=${PIPELINE_VERSION} ${MAVEN_ARGS}
     if [[ "${CI}" == "CONCOURSE" ]]; then
         ./mvnw clean verify deploy -Ddistribution.management.release.id=${M2_SETTINGS_REPO_ID} -Ddistribution.management.release.url=${REPO_WITH_JARS} ${MAVEN_ARGS} || ( $( printTestResults ) && return 1)
     else
--- concourse/login.sh
+++ concourse/login.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ROOT_ADDRESS=${1:-localhost}
+#ROOT_ADDRESS=${1:-localhost}
+ROOT_ADDRESS=${1:-10.244.8.2}
 
-./fly -t docker login -c http://${ROOT_ADDRESS}:8080 -u=concourse -p=changeme
+fly -t docker login -c http://${ROOT_ADDRESS}:8080
+#fly -t docker login -c http://${ROOT_ADDRESS}:8080 -u=concourse -p=changeme
